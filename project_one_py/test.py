66f82ac631195e6396398cd23522a183b7c28595
Ryan Pascal Desktop <ryanpascal99@yahoo.com>
2019-02-24 17:57:09 -0500

    removing old

--- project_one_py/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-from sklearn.cross_validation import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn import tree
