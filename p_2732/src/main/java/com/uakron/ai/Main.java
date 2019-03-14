6ffaa76df1a0b3aa1a0e0df9384bf6aa2b663b28
Ryan <ryan.david.pascal@gmail.com>
2019-03-13 20:13:53 -0400

    adding shit

--- p_2732/src/main/java/com/uakron/ai/Main.java
+++ p_2732/src/main/java/com/uakron/ai/Main.java
@@ -14,11 +14,10 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 
-class ListOptions {
-    public String node;
-    public String nextNode;
-    public String result;
-    public List<ListOptions> options;
+class Model {
+    public String id;
+    public String label;
+    public List<Model> children;
     public String finalValue;
 }
 
@@ -73,9 +72,11 @@ public class Main {
                         j48Model.buildClassifier(dataSet);
 
 
+//                        printGroup(j48Model.toString());
+
                         optionThree();
 
-//                        Enumeration<Option> o = j48Model.listOptions();
+//                        Enumeration<Option> o = j48Model.Model();
 //                        while(o.hasMoreElements())
 //                        {
 //                            Option oo = o.nextElement();
@@ -128,22 +129,48 @@ public class Main {
                         }
 
 
-                        Instances newCases = getNewAttributes();
+//                        String currentNode = tree.get(0).substring(0, 2);
+//
+//                        boolean hasChildren = true;
+//                        while(hasChildren){
+//                            String value = readFromConsole(currentNode);
+//
+//                            Optional<String> newLine = tree.stream().filter((item) ->{
+//                                return item.contains(currentNode) && item.contains(value);
+//                            }).findFirst();//.collect(Collectors.toList());
+//
+//                            if(newLine.isPresent()){
+//                                printGroup("Try entering new value that didnt work...");
+//                                continue;
+//                            }
+//
+//
+//                            String newNode = newLine.get().substring(4, 2);
+//
+//
+//
+//
+//
+//
+//                        }
 
-                        try {
-                            Evaluation eval = new Evaluation(newCases);
 
-                            if (newCases.numInstances() >= 10 && readFromConsole("Cross Validate? (y/n): ").toLowerCase().equals("y")) {
-                                eval.crossValidateModel(j48Model, newCases, 10, new Random(1));
-                            } else {
-                                eval.evaluateModel(j48Model, newCases);
-                            }
-
-                            printGroup(eval.toSummaryString("Results:\n", true));
-                            printGroup(eval.toMatrixString());
-                        } catch (Exception e) {
-                            printGroup(e.toString(), "Unable to apply the decision tree to entered cases");
-                        }
+//                        Instances newCases = getNewAttributes();
+//
+//                        try {
+//                            Evaluation eval = new Evaluation(newCases);
+//
+//                            if (newCases.numInstances() >= 10 && readFromConsole("Cross Validate? (y/n): ").toLowerCase().equals("y")) {
+//                                eval.crossValidateModel(j48Model, newCases, 10, new Random(1));
+//                            } else {
+//                                eval.evaluateModel(j48Model, newCases);
+//                            }
+//
+//                            printGroup(eval.toSummaryString("Results:\n", true));
+//                            printGroup(eval.toMatrixString());
+//                        } catch (Exception e) {
+//                            printGroup(e.toString(), "Unable to apply the decision tree to entered cases");
+//                        }
                         break;
                     }
                     case 5: {
@@ -162,32 +189,63 @@ public class Main {
 
 
     private void optionThree() throws Exception {
+
+
+        List<Model> tre = new ArrayList<Model>();
+
         List<String> tree = new ArrayList<String>(Arrays.asList(j48Model.graph().split("\n")));
         tree.remove(0);
         tree.remove(tree.size() - 1);
-        List<ListOptions> options = new ArrayList<>();
+        String root = tree.get(0).substring(0, 2);
 
-        String firstNode = tree.get(0).substring(0, 2);
+        printGroup("Root:", root);
+
 
         tree.remove(0);
 
-        List<ListOptions> res = getNodes(firstNode, tree);
 
-//        for (String s : tree) {
-//            s = s.trim();
-//            String node = s.substring(0, 2);
-//            printGroup(node);
-//        }
+        tree.forEach(item -> {
+            printGroup(item);
+
+            String parent = item.substring(0, 2);
+            String next = item.substring(4, 6);
+
+            String[] chars = next.split("");
+
+            if(chars[0] == "N" && chars[1].matches("-?(0|[1-9]\\d*)")){
+                printGroup("Terminating node", next, parent);
+            }
+
+
+            printGroup("--------------");
+        });
+
+//        List<String> tree = new ArrayList<String>(Arrays.asList(j48Model.graph().split("\n")));
+//        tree.remove(0);
+//        tree.remove(tree.size() - 1);
+//        List<Model> options = new ArrayList<>();
+//
+//        String firstNode = tree.get(0).substring(0, 2);
+//
+//        tree.remove(0);
+//
+//        List<Model> res = getNodes(firstNode, tree);
+//
+////        for (String s : tree) {
+////            s = s.trim();
+////            String node = s.substring(0, 2);
+////            printGroup(node);
+////        }
     }
 
-    private List<ListOptions> getNodes(String node, List<String> lines){
-        List<String> newLines = lines.stream().filter((item) ->{
+    private List<Model> getNodes(String node, List<String> lines) {
+        List<String> newLines = lines.stream().filter((item) -> {
             return item.contains(node);
         }).collect(Collectors.toList());
 
-        List<ListOptions> options = new ArrayList<>();
+        List<Model> options = new ArrayList<>();
 
-        newLines.forEach(item ->{
+        newLines.forEach(item -> {
 
 
         });
--- project_1/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -cp target/project_1-1.0-jar-with-dependencies.jar com.uakron.ai.Main
